8adfdb7a973b75273ff2778c8a8ef8090a7f938c
rdotr57 <92831976+ALWEB2@users.noreply.github.com>
2021-11-30 21:37:20 -0500

    open in new tab
    a

--- new/postlink.php
+++ new/postlink.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['name'])){
         exit();
     } elseif (in_array($link, array("nigger", "n1gger", "N1gger", "Nigger", " ", "", "­", "­­"))){
     } else {
-	$link_message = "<div class='msgln'><b class='user-name'>".$_SESSION['name']."</b> <a href=$link>$link</a> </div>";
+	$link_message = "<div class='msgln'><b class='user-name'>".$_SESSION['name']."</b> <a href=$link target=\"_blank\">$link</a> </div>";
     file_put_contents("log.html", $link_message, FILE_APPEND | LOCK_EX);
     }
 }

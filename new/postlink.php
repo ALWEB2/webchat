b464405d57cbfd14b236f0552d03ddf8931c5655
rdotr57 <92831976+ALWEB2@users.noreply.github.com>
2021-11-30 21:25:24 -0500

    fixed empy space glitch again
    a

--- new/postlink.php
+++ new/postlink.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['name'])){
     $link = $_POST['link'];
     if (in_array($_SESSION['name'], array("Server", "server", "Serveur", "serveur"))){
         exit();
-    } elseif (in_array($link, array("nigger", "n1gger", "N1gger", "Nigger", " ", ""))){
+    } elseif (in_array($link, array("nigger", "n1gger", "N1gger", "Nigger", " ", "", "­", "­­"))){
     } else {
 	$link_message = "<div class='msgln'><b class='user-name'>".$_SESSION['name']."</b> <a href=$link>$link</a> </div>";
     file_put_contents("log.html", $link_message, FILE_APPEND | LOCK_EX);
